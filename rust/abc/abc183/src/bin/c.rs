779e6b7718a818764de2879a0e430c3746c627f8
Koyashiro Kohaku <develop@koyashiro.net>
2020-11-15 23:11:29 +0900

    refactor: update  abc183c

--- rust/abc/abc183/src/bin/c.rs
+++ rust/abc/abc183/src/bin/c.rs
@@ -9,13 +9,16 @@ fn main() {
         t: [[usize; n]; n],
     }
 
-    let mut routes: Vec<Vec<usize>> = vec![];
-    for i in (2..n + 1).permutations(n - 1) {
-        let mut route = vec![1];
-        for n in i {
-            route.push(n);
-        }
+    let mut routes: Vec<Vec<usize>> = Vec::new();
+    for permutation in (2..n + 1).permutations(n - 1) {
+        let mut route = Vec::new();
+
         route.push(1);
+        permutation.iter().for_each(|i| {
+            route.push(*i);
+        });
+        route.push(1);
+
         routes.push(route);
     }
 
@@ -24,9 +27,9 @@ fn main() {
     for route in routes {
         let mut dt: usize = 0;
         for n in 0..route.len() - 1 {
-            let from: usize = route[n];
-            let to: usize = route[n + 1];
-            let t: usize = t[(from - 1) as usize][(to - 1) as usize];
+            let from = route[n];
+            let to = route[n + 1];
+            let t = t[from - 1][to - 1];
             dt += t;
         }
         if dt == k {
